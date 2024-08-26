7aeda35dc838039b49e31aa7caeb08d4c55e6805
mazynoah <mazynoah@gmail.com>
2024-08-26 15:56:58 +0200

    chore: fix comment code blocks

--- src/lib.rs
+++ src/lib.rs
@@ -34,9 +34,10 @@ pub enum StoreError {
     Write(#[source] std::io::Error),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub enum StoringType {
     Cache,
+    #[default]
     Data,
     Config,
 }
@@ -87,54 +88,53 @@ impl<T: Storing> StoreHandle<T> {
 /// # Example
 ///
 /// ```
-///use serde::{Deserialize, Serialize};
-///use storage::{Storage, StoreHandle, Storing, StoringType};
+/// use serde::{Deserialize, Serialize};
+/// use storage::{Storage, StoreHandle, Storing, StoringType};
 ///
-///#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Default)]
-///pub struct MyStore {
-///    pub count: u32,
-///}
+/// #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Default)]
+/// pub struct MyStore {
+///     pub count: u32,
+/// }
 ///
-///impl MyStore {
-///    fn increment_count(&mut self) {
-///        self.count += 1;
-///    }
-///}
+/// impl MyStore {
+///     fn increment_count(&mut self) {
+///         self.count += 1;
+///     }
+/// }
 ///
-///impl Storing for MyStore {
-///    fn store_type() -> StoringType {
-///        StoringType::Data
-///    }
-///}
+/// impl Storing for MyStore {
+///     fn store_type() -> StoringType {
+///         StoringType::Data
+///     }
+/// }
 ///
-///fn main() {
-///    // Initialize the Storage with the defaults
-///    let storage = Storage::new("com.github.mazynoah.storage".to_owned());
+/// // Initialize the Storage with the defaults
+/// let storage = Storage::new("com.github.mazynoah.storage".to_owned());
 ///
-///    // Create a handle for managing the store data.
-///    let mut handle = StoreHandle::<MyStore>::new("handle");
+/// // Create a handle for managing the store data.
+/// let mut handle = StoreHandle::<MyStore>::new("handle");
 ///
-///    // Read existing store from storage
-///    storage
-///        .read(&mut handle)
-///        .expect("Failed to read from storage");
+/// // Read existing store from storage
+/// storage
+///     .read(&mut handle)
+///     .expect("Failed to read from storage");
 ///
-///    // Modify the store data
-///    let counter = handle.get_store_mut();
+/// // Modify the store data
+/// let counter = handle.get_store_mut();
 ///
-///    counter.increment_count();
-///    counter.increment_count();
-///    counter.increment_count();
+/// counter.increment_count();
+/// counter.increment_count();
+/// counter.increment_count();
 ///
-///    // Write changes to disk
-///    storage
-///        .write(&mut handle)
-///        .expect("Failed to write to storage");
+/// // Write changes to disk
+/// storage
+///     .write(&mut handle)
+///     .expect("Failed to write to storage");
 ///
-///    let counter = handle.get_store();
+/// let counter = handle.get_store();
 ///
-///    println!("Count: {}", counter.count);
-///}
+/// println!("Count: {}", counter.count);
+///
 ///
 /// ```
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
